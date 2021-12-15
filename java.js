e9e57065228f7717ff164dac9b4baa32e498075d
fl1p <bishu.hover@gmail.com>
2021-12-16 01:17:36 +0800

    fl1p

---
+++ java.js
@@ -0,0 +1,9 @@
+// function enableScroll() {
+//     window.onscroll = function() {};
+// }
+// window.onload = function() {
+//     if (!window.location.hash) {
+//         window.location = window.location + '#loaded';
+//         window.location.reload();
+//     }
+// }
